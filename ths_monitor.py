479fcda076ca3507aed89aa6d4a8e23e33ebfb33
frankyzhou <lijunzhou-fudan@qq.com>
2016-11-06 14:07:11 +0800

    init

--- ths_monitor.py
+++ ths_monitor.py
@@ -3,7 +3,7 @@ import datetime
 import socket
 import time
 import traceback
-
+import sys
 
 def get_client(host='127.0.0.1', textport=51500, timeout=15):
     # 如果超时一般都是交易端有故障，抛出异常，邮件提示
@@ -32,27 +32,27 @@ c = client(host="127.0.0.1")
 while 1:
     try:
         print "Enter data to transmit: stop, get_position, buy, sell"
-        # data = sys.stdin.readline().strip()
-        time.sleep(5)
+        data = sys.stdin.readline().strip()
+        # time.sleep(5)
         # data = "get_position 0001"
-        data = "buy 000001 10.0 2000000"
+        # data = "buy 000001 10.0 2000000"
         print data
         print datetime.datetime.now().strftime("%H:%M:%S")
         c.exec_order(data)
         print datetime.datetime.now().strftime("%H:%M:%S")
 
-        time.sleep(5)
-        data = "sell 300072 10.0 200000"
-        print data
-        print datetime.datetime.now().strftime("%H:%M:%S")
-        c.exec_order(data)
-        print datetime.datetime.now().strftime("%H:%M:%S")
-
-        time.sleep(5)
-        data = "sell 000001 10.0 200000"
-        print data
-        print datetime.datetime.now().strftime("%H:%M:%S")
-        c.exec_order(data)
-        print datetime.datetime.now().strftime("%H:%M:%S")
+        # time.sleep(5)
+        # data = "sell 300072 10.0 200000"
+        # print data
+        # print datetime.datetime.now().strftime("%H:%M:%S")
+        # c.exec_order(data)
+        # print datetime.datetime.now().strftime("%H:%M:%S")
+        #
+        # time.sleep(5)
+        # data = "sell 000001 10.0 200000"
+        # print data
+        # print datetime.datetime.now().strftime("%H:%M:%S")
+        # c.exec_order(data)
+        # print datetime.datetime.now().strftime("%H:%M:%S")
     except :
         traceback.print_exc()
